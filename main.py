532c0b688145ed3696581945527fe97fe033d3d5
David Gomez <doga19941@hotmail.com>
2023-03-18 23:24:33 -0500

    Funcion SUPER

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from persona import Cliente
+from persona import Cliente, Empleado
 
-cliente1 = Cliente('David', 29)
-cliente2 = Cliente('Orlando', 25)
+#cliente1 = Cliente('David', 29)
+#cliente2 = Cliente('Orlando', 25)
 
-cliente1.detalle_persona()
+#cliente1.detalle_persona()
+#print(cliente2)
 
-print(cliente2)
+empleado1 = Empleado('Pinky', 12, 2500000)
+empleado2 = Empleado('Kaiser', 9, 1500000)
+
+empleado1.detalle_persona()
+empleado1.detalle_empleado()
+
+print(empleado2)
--- persona.py
+++ persona.py
@@ -9,5 +9,19 @@ class Persona:
     def __str__(self):
         return f'Nombre: {self.nombre} \nEdad: {self.edad}'
     
+#Clase cliente que hereda de persona
 class Cliente(Persona):
     pass
+
+#Clase Empleado que hereda de Persona
+class Empleado(Persona):
+    def __init__(self, nombre, edad, sueldo):
+        super().__init__(nombre, edad)
+        self.sueldo = sueldo
+
+    def detalle_empleado(self):
+        super().detalle_persona()
+        print('Sueldo:',self.sueldo)
+
+    def __str__(self):
+        return super().__str__() + f'\nSueldo: {self.sueldo}'
